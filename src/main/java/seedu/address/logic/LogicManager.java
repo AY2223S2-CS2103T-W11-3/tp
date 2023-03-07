319e96694d62cb2ab0e1c2b88b269d528796f164
Nathan Tran <80334013+rockman007372@users.noreply.github.com>
2023-03-08 00:39:03 +0800

    Add and modify Use Cases

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -46,7 +46,7 @@ public class LogicManager implements Logic {
         commandResult = command.execute(model);
 
         try {
-            storage.saveAddressBook(model.getAddressBook());
+            storage.saveAddressBook(model.getAddressBook()); // Save after every command
         } catch (IOException ioe) {
             throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
         }
